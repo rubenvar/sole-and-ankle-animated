b584c1ffc249b567c7df7f837cac24eedb405583
Rubén <hey@rubenvara.io>
2021-12-16 00:29:15 +0100

    mobile menu animation

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import styled from 'styled-components/macro';
+import styled, { keyframes } from 'styled-components/macro';
 import { DialogOverlay, DialogContent } from '@reach/dialog';
 
 import { QUERIES, WEIGHTS } from '../../constants';
@@ -19,22 +19,40 @@ const MobileMenu = ({ isOpen, onDismiss }) => {
         </CloseButton>
         <Filler />
         <Nav>
-          <NavLink href="/sale">Sale</NavLink>
-          <NavLink href="/new">New&nbsp;Releases</NavLink>
-          <NavLink href="/men">Men</NavLink>
-          <NavLink href="/women">Women</NavLink>
-          <NavLink href="/kids">Kids</NavLink>
-          <NavLink href="/collections">Collections</NavLink>
+          <NavLink i={0} href="/sale">Sale</NavLink>
+          <NavLink i={1} href="/new">New&nbsp;Releases</NavLink>
+          <NavLink i={2} href="/men">Men</NavLink>
+          <NavLink i={3} href="/women">Women</NavLink>
+          <NavLink i={4} href="/kids">Kids</NavLink>
+          <NavLink i={5} href="/collections">Collections</NavLink>
         </Nav>
         <Footer>
-          <SubLink href="/terms">Terms and Conditions</SubLink>
-          <SubLink href="/privacy">Privacy Policy</SubLink>
-          <SubLink href="/contact">Contact Us</SubLink>
+          <SubLink i={6} href="/terms">Terms and Conditions</SubLink>
+          <SubLink i={7} href="/privacy">Privacy Policy</SubLink>
+          <SubLink i={8} href="/contact">Contact Us</SubLink>
         </Footer>
       </Content>
     </Overlay>
   );
 };
+
+const appear = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+const slide = keyframes`
+  from {
+    transform: translateX(300px);
+  }
+  to {
+    transform: translateX(0);
+  }
+`;
 
 const Overlay = styled(DialogOverlay)`
   position: fixed;
@@ -45,6 +63,8 @@ const Overlay = styled(DialogOverlay)`
   background: var(--color-backdrop);
   display: flex;
   justify-content: flex-end;
+  animation: ${appear} 0.6s;
+  animation-fill-mode: forwards;
 `;
 
 const Content = styled(DialogContent)`
@@ -54,6 +74,7 @@ const Content = styled(DialogContent)`
   padding: 24px 32px;
   display: flex;
   flex-direction: column;
+  animation: ${slide} 0.25s cubic-bezier(0, 0, 0.65, 1.23);
 `;
 
 const CloseButton = styled(UnstyledButton)`
@@ -67,7 +88,7 @@ const Nav = styled.nav`
   display: flex;
   flex-direction: column;
   gap: 16px;
-`;
+  `;
 
 const NavLink = styled.a`
   color: var(--color-gray-900);
@@ -75,6 +96,12 @@ const NavLink = styled.a`
   text-decoration: none;
   font-size: 1.125rem;
   text-transform: uppercase;
+  
+  /* each link has an (incremental) i value passed in props */
+  /* use that index for orchestration */
+  animation: ${appear} 0.2s;
+  animation-delay: calc(${(props) => props.i * 0.075}s + 0.2s);
+  animation-fill-mode: both;
 
   &:first-of-type {
     color: var(--color-secondary);
@@ -84,6 +111,7 @@ const NavLink = styled.a`
 const Filler = styled.div`
   flex: 1;
 `;
+
 const Footer = styled.footer`
   flex: 1;
   display: flex;
@@ -96,6 +124,10 @@ const SubLink = styled.a`
   color: var(--color-gray-700);
   font-size: 0.875rem;
   text-decoration: none;
+
+  animation: ${appear} 0.2s;
+  animation-delay: calc(${(props) => props.i * 0.075}s + 0.2s);
+  animation-fill-mode: both;
 `;
 
 export default MobileMenu;
